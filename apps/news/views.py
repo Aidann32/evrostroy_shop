4668f99cebf41004fe592bff34b8b4c8d518cc91
Aidann32 <aliaidan02@gmail.com>
2026-08-20 00:24:09 +0500

    Added news pages and search result

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
 
 def detail(request, slug):
-    pass
+    news_item = get_object_or_404(News, slug=slug, is_published=True)
+    return render(request, 'news/news_details.html', {'item': news_item})
 
 def news_list(request):
     return render(request, 'news/all_news.html', {'news': News.objects.filter(is_published=True)})
